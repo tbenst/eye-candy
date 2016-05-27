029555e98afd7ea34b55e15f1c63bf6971713640
tbenst <tbenst@gmail.com>
2016-05-27 14:35:51 -0400

    router implemented

--- web/src/app.js
+++ web/src/app.js
@@ -1,8 +1,31 @@
 const Koa = require('koa');
-const app = new Koa();
+const router = require('koa-router')();
+const path = require('path')
 
-app.use(ctx => {
-  ctx.body = 'Hello World';
+import co from 'co';
+import render from 'koa-ejs';
+
+
+const app = new Koa();
+render(app, {
+  root: path.join(__dirname, '../view'),
+  layout: 'template',
+  viewExt: 'html',
+  cache: false,
+  debug: true
 });
+
+app.context.render = co.wrap(app.context.render);
+
+router.get('/', async ctx => {
+    await ctx.render('stimulus');
+    // ctx.body = 'Hello ty';
+});
+
+
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000);
