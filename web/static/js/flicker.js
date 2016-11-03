a045f52e7a1a14cb79251bb163cf938b20e345ef
Tyler Benster <tbenst@gmail.com>
2016-11-02 19:38:43 -0700

    gamma correction

--- web/static/js/flicker.js
+++ web/static/js/flicker.js
@@ -1,22 +1,69 @@
-const canvas=document.getElementById("flicker")
+// const canvas=document.getElementById("flicker")
+// var context = canvas.getContext("2d")
+
+// canvas.width  = window.innerWidth
+// canvas.height = window.innerHeight
+
+// function renderLoop() {
+//     if (context.getImageData(1,1,1,1).data[0]===255) {
+//         localStorage.setItem("newStimulus", false)
+//     }
+    
+//     if (localStorage.getItem("newStimulus")==="true") {
+//         context.fillStyle = "white"
+//         context.fillRect(0, 0, canvas.width, canvas.height);
+//         // verify it was drawn
+//     } else {
+//         context.fillStyle = localStorage.getItem("signalLight")
+//         context.fillRect(0, 0, canvas.width, canvas.height);       
+//     }
+//     requestAnimationFrame(renderLoop)
+// }
+
+// renderLoop()
+
+
+
+
+var canvas=document.getElementById("flicker")
 var context = canvas.getContext("2d")
 
 canvas.width  = window.innerWidth
 canvas.height = window.innerHeight
 
+function numberToColor(f) {
+    // gamma compress linear light intensity between zero and one
+    let n = Math.ceil((1.055*Math.pow(f,1/2.4)-0.055)*255)
+    let hex = n.toString(16)
+    return "#"+hex+hex+hex
+}
+
+let n
+let s
+let stimulusIndex
+let frame
+// once per second
 function renderLoop() {
-    if (context.getImageData(1,1,1,1).data[0]===255) {
-        localStorage.setItem("newStimulus", false)
+    stimulusIndex = localStorage.getItem("stimulusIndex")
+    frame = localStorage.getItem("frame")
+
+    if (frame == "FRAME_A") {
+        n = -0.075
+    } else if (frame == "FRAME_B") {
+        n = 0.075
     }
     
-    if (localStorage.getItem("newStimulus")==="true") {
-        context.fillStyle = "white"
-        context.fillRect(0, 0, canvas.width, canvas.height);
-        // verify it was drawn
-    } else {
-        context.fillStyle = localStorage.getItem("signalLight")
-        context.fillRect(0, 0, canvas.width, canvas.height);       
+    if (stimulusIndex%3===0) {
+        s = 0.8
+    } else if (stimulusIndex%3===1) {
+        s = 0.5
+    } else if (stimulusIndex%3===2) {
+    s = 0.2
     }
+
+    context.fillStyle = numberToColor(s+n)
+    context.fillRect(0, 0, canvas.width, canvas.height);       
+    i++
     requestAnimationFrame(renderLoop)
 }
 
