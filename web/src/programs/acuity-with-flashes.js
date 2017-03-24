8dc342d4878eba39097a7cc2ca25ef45c650f158
Tyler Benster <tbenst@gmail.com>
2017-03-24 11:54:18 -0700

    added kinetics

---
+++ web/src/programs/acuity-with-flashes.js
@@ -0,0 +1,36 @@
+const metadata = {name: "acuity-with-flashes", version: "0.3.2"}
+
+let widths = [...Array(12).keys()].map(x => (x+1)*10)
+let speeds = [...Array(8).keys()].map(x => (1+x)*100)
+// 12 angles, offset by 22 degrees to reduce diamond artifact
+let angles = [...Array(12).keys()].map(x => (x*2+1)*PI/12)
+let stimuli = []
+
+let width
+let lifespan
+let id
+
+for (let speed of speeds) {
+	for (let width of widths) {
+		lifespan = calcBarLifespan(speed,width,windowHeight,windowWidth)
+		id = r.uuid()
+
+		for (let angle of angles) {
+			integrityMeta = {group: r.uuid(), label: "integrity"}
+			stimuli.push([
+				new Wait(120, integrityMeta),
+				new Solid(120, "white", integrityMeta),
+				new Wait(120, integrityMeta)
+				new Bar(lifespan,"black", speed, width, angle, "white",
+					{group: id}),
+			])
+		}
+	}
+}
+
+r.shuffle(stimuli)
+
+let stimulusGenerator = flatten(stimuli)
+for (let s of stimulusGenerator) {
+	yield s
+}
