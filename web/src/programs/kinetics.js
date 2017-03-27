e5262a75b15b213a4cca2fe8f4e1404b58e9c655
Tyler Benster <tbenst@gmail.com>
2017-03-27 15:44:49 -0700

    new kinetics v0.2.0

--- web/src/programs/kinetics.js
+++ web/src/programs/kinetics.js
@@ -1,29 +1,31 @@
-const metadata = {name: "kinetics", version: "0.1.0"}
+const metadata = {name: "kinetics", version: "0.2.0"}
 
 let stimuli = []
-let group
+let group = []
 let meta
+let onTime = 120
 
 // "kinetic wedge" of different gap sizes
 // .1 second steps through 6 seconds
 for (let i=12; i<=6*120; i=i+12) {
 	meta = {group: r.uuid()}
-	group = [
-		new Wait(120, meta),
-		new Solid(120, "white", meta),
-		new Wait(i, meta),
-		new Solid(120, "white", meta),
-		new Wait(120, meta)
-	]
+	group = []
+	group.push(new Wait(120, meta))
+	// 5 repetitions
+	group.push(new Solid(onTime, "white", meta))
+	for (let j = 0; j < 4; j++) {
+		group.push(new Wait(i, meta))
+		group.push(new Solid(onTime, "white", meta))
+	}
+	group.push(new Wait(120, meta))
+
 	stimuli.push(group)
 }
 
-// 5 repetitions
-for (let i = 0; i < 5; i++) {
-	r.shuffle(stimuli)
-	for (let group of stimuli) {
-		for (let stimulus of group) {
-			yield stimulus
-		}
+
+r.shuffle(stimuli)
+for (let group of stimuli) {
+	for (let stimulus of group) {
+		yield stimulus
 	}
 }
