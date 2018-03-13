7653d4f96e41cbf523cf0488fe886070fab65647
Tyler Benster <tbenst@gmail.com>
2018-03-13 00:17:29 -0700

    new speed & contrast with break and only AA and AB

--- web/src/programs/grating-contrast.js
+++ web/src/programs/grating-contrast.js
@@ -1,17 +1,18 @@
-const metadata = {name: "checkerboard-contrast", version: "0.4.0", inverted: false}
-// 0.3 introduces new gamma compression
+const metadata = {name: "grating-contrast", version: "0.1.0"}
+
 
 let repetitions = 25
-let durations = [0.5]
+let durations = [1]
 // 0 is 1 (max) contrast, -1 is 0.1 contrast, -2 is 0.01
 // -2.2 is minimal contrast, <=-2.3 is same color for 8 bit color
 let startLogContrast = 0
 let logContrastStep = -0.1
 let ncontrasts = 5
-let angle = PI/4
+let angles = [PI/4]
+let speed = 100
 let nsizes = 6
-let startLogMAR = 2.1
-let logMarStep = 0.1
+let startLogMAR = 1.8
+let logMarStep = 0.2
 
 
 function linearToHex(f) {
@@ -33,10 +34,15 @@ function logMARtoPx(logMAR, pxPerDegree=12.524) {
     return round(degrees*pxPerDegree)
 }
 
+function inverseAngle(angle) {
+    return (angle + PI) % (2*PI)
+}
+
 
 let sizes = [...Array(nsizes).keys()].map(
     x => x*logMarStep+startLogMAR).map(
     x => logMARtoPx(x))
+
 
 
 let colors = [...Array(ncontrasts).keys()].map(
@@ -64,7 +70,6 @@ function* measureIntegrity(stimuli,every=5*60) {
     }
 }
 
-
 function* insertBreaks(stimuli,every=10*60) {
     // every N seconds, do a WAIT
     let integrityMeta
@@ -82,62 +87,36 @@ function* insertBreaks(stimuli,every=10*60) {
     }
 }
 
-function checkerboard_group(class1, class2, duration, size, cohort, colorPair) {
-    const id = r.uuid()
-    let target
-    let color1
-    let color2
-    if (class1=='A') {
-        color1 = colorPair
-    } else {
-        color1 = [colorPair[1], colorPair[0]]
-    }
-
-    if (class2=='A') {
-        color2 = colorPair
-    } else {
-        color2 = [colorPair[1], colorPair[0]]
-    }
-
-    if (class1===class2) {
-        target = 'SAME'
-    } else {
-        target = 'DIFFERENT'
-    }
-    const before = new Wait(1, {group: id})
-    const first = new Checkerboard(duration, color1[0], color1[1], size, angle,
-        {group: id, cohort: cohort, block: true, class: class1,
-                   target: target})
-    const second = new Checkerboard(duration, color2[0], color2[1], size, angle,
-        {group: id, cohort: cohort, block: true, class: class2,
-                   target: target})
-    const after = new Wait(r.randi(1,1.5), {group: id, block: true})
-    return [before, first, second, after]
-}
-
-let x
-let y
 let stimuli = []
-let l
+let left
+let right
 let before
 let after
 let id
 let cohort
 
 for (let size of sizes) {
-    for (let colorPair of colors) {
-        for (let duration of durations) {
-            for (let i = 0; i < repetitions; i++) {
-                // use cohort to maintain balance in analysis
-                cohort = r.uuid()
-                stimuli.push(checkerboard_group('A','B', duration,size,
-                    cohort, colorPair))
-                stimuli.push(checkerboard_group('A','A', duration,size,
-                    cohort, colorPair))
-                // stimuli.push(checkerboard_group('B','B', duration,size,
-                    // cohort, colorPair))
-                // stimuli.push(checkerboard_group('B','A', duration,size,
-                    // cohort, colorPair))
+    for (let angle of angles) {
+        for (let colorPair of colors) {
+            for (let duration of durations) {
+                for (let i = 0; i < repetitions; i++) {
+                    // use cohort to maintain balance in analysis
+                    cohort = r.uuid()
+                    before = new Wait(1, {group: id})
+
+                    id = r.uuid()
+                    left = new Grating(duration,colorPair[0], speed, size, angle, colorPair[1],
+                        {group: id, cohort: cohort, class: "FORWARD", block: true})
+                    after = new Wait(r.randi(1,1.5), {group: id, block: true})
+                    stimuli.push([before,left,after])
+
+                    id = r.uuid()
+                    meta = {group: id, block: true}
+                    right = new Grating(duration,colorPair[0], speed, size, inverseAngle(angle), colorPair[1],
+                        {group: id, cohort: cohort, class: "REVERSE", block: true})
+                    after = new Wait(r.randi(1,1.5), {group: id, block: true})
+                    stimuli.push([before,right,after])
+                }
             }
         }
     }
