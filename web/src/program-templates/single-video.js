427ed500ac6fac549bcad51e37d58722792472be
Tyler Benster <tbenst@gmail.com>
2019-04-04 00:10:10 -0700

    added repetitions option to single-video

--- web/src/program-templates/single-video.js
+++ web/src/program-templates/single-video.js
@@ -3,10 +3,15 @@ const metadata = {name: "single-video", version: "0.1.0"}
 let stimuli = []
 let meta = {}
 
+let repetitions = 1
+
 // custom template of dollar sign with curly, will regex sub
 // TODO hardcoded for godfather...
-stimuli.push(new Video(5, "black", "${VID_SRC}", meta))
-// stimuli.push(new Video(262, "black", "${VID_SRC}", meta))
+// stimuli.push(new Video(5, "black", "${VID_SRC}", meta))
+
+for (var i = 0; i < repetitions; i++) {
+    stimuli.push(new Video(262, "black", "${VID_SRC}", meta))
+}
 
 
 function* stimulusGenerator(renderResults) {
