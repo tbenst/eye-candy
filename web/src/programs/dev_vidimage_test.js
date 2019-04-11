3abc94c7c0f074397ad8f70addfe969c28c7c662
Tyler Benster <tbenst@gmail.com>
2019-04-11 00:33:38 -0700

    new dev_vidimage_test

--- web/src/programs/dev_vidimage_test.js
+++ web/src/programs/dev_vidimage_test.js
@@ -3,16 +3,15 @@ const metadata = {name: "single-video", version: "0.1.0"}
 let stimuli = []
 let meta = {}
 
-let repetitions = 5
+let repetitions = 10
 
 // custom template of dollar sign with curly, will regex sub
 // TODO hardcoded for godfather...
-// stimuli.push(new Video(5, "black", "videos/godfather_michael_4min.mp4", meta))
 
 for (var i = 0; i < repetitions; i++) {
     // Video(lifespan, backgroundColor, src, startTime, metadata)
-    stimuli.push(new Video(5, "black", "videos/godfather_michael_4min.mp4", 0, meta))
-stimuli.push(new Image(5, "black", "images/fish.png", undefined, meta))
+    stimuli.push(new Video(5, "black", "videos/godfather_michael_4min.mp4", 5*i, meta))
+    stimuli.push(new Image(5, "black", "images/scary_hawk.jpg", undefined, meta))
 }
 
 
