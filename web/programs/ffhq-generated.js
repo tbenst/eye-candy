f9156f523bccaea1faa423e8716b0ed1a7f08957
Tyler Benster <tbenst@gmail.com>
2020-07-21 05:09:57 -0700

    fixed ffhq-generated protocol

--- web/programs/ffhq-generated.js
+++ web/programs/ffhq-generated.js
@@ -1,12 +1,12 @@
-const metadata = {name: "ffhq-generated", version: "0.2.1"}
+const metadata = {name: "ffhq-generated", version: "0.1.0"}
 const duration = 0.5
 const repetitions = 1
-const scaleX = 1.125
-const scaleY = 1.125
-let images = [...Array(10000).keys()].map(x => String(x))
+const scaleX = 1.1
+const scaleY = 1.1
+let images = [...Array(100).keys()].map(x => String(x).padStart(4,"0"))
 
 // First 5000 images only
-images = images.slice(0,5000)
+// images = images.slice(0,5000)
 
 // Second 5000 images only
 // images = images.slice(5000,10000)
@@ -17,7 +17,7 @@ const imageExt = ".png"
 let stimuli = []
 let id, cohort
 // TODO fixationPoint is broken / unintuitive
-let fixationPoint = {x: 600*scaleX/2, y: 360*scaleY/2} // center the 260 x 360 image
+let fixationPoint = {x: 640*scaleX/2, y: 640*scaleY/2} // center the 260 x 360 image
 const classLabels = ["seed"]
 for (let rep = 0; rep < repetitions; rep++) {
   cohort = r.uuid()
@@ -35,9 +35,10 @@ for (let rep = 0; rep < repetitions; rep++) {
 }
 
 r.shuffle(stimuli)
-// stimuli = measureIntegrity(stimuli)
+stimuli = flatten(stimuli)
+stimuli = measureIntegrity(stimuli)
 
-let celltyping_stimuli = celltyping(r, windowHeight, windowWidth, 25,40)()
+// let celltyping_stimuli = celltyping(r, windowHeight, windowWidth, 25,40)()
 // var stimulusGenerator = concat_generators(celltyping_stimuli, stimuli)
 // stimulusGenerator = stimuli
 // stimulusGenerator = celltyping_stimuli
